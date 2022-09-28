51b07e697408147c5284f74826867459408011cd
Alton Fo <altonfo109@gmail.com>
2022-09-28 02:46:47 -0400

    commit tree c365c868d2bff50292e3090a1f5b552dabe8c3f7

---
+++ index.js
@@ -0,0 +1,39 @@
+function add() {
+    return x+y
+}
+function subtract() {
+
+}
+function multiply() {
+
+}
+function divide() {
+
+}
+function add(x, y) {
+    return x+y
+}
+function subtract(a, b) {
+   console.log(30,7);
+    return a-b 
+}
+function multiply(a, b) {
+    console.log(2,60970);
+     return a*b
+} 
+function divide(a, b) {
+    console.log(11.027227722772277, 10);
+    return a/b
+}
+function increment(n) {
+    return ++n
+}
+function decrement(n) {
+    return --n
+}
+function makeInt(n) {
+    return parseInt(n,10)
+}
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
